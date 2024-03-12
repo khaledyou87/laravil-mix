a92a975286e3d459c6c7578b405e2551c51c9cc8
khaledyou87 <66491012+khaledyou87@users.noreply.github.com>
2024-03-13 00:29:00 +0300

    3

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// alert("hello world");
+import * as bootstrap from "bootstrap";
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", () => {
+    const toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
